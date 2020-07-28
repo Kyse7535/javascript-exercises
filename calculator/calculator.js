dc841e3355d53eabdaf3fc13c77f37f4efa1efc3
Kyse7535 <56298254+Kyse7535@users.noreply.github.com>
2020-07-28 12:29:33 +0200

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,43 @@
-function add () {
-	
-}
+function add(valeur1, valeur2) {
+            return valeur1 + valeur2
+        }
 
-function subtract () {
-	
-}
+        function subtract(valeur1, valeur2) {
+            return valeur1 - valeur2;
+        }
 
-function sum () {
-	
-}
+        function mulpliply(valeur1, valeur2) {
+            return valeur * valeur2;
+        }
 
-function multiply () {
-	
-}
+        function sum(valeur1, valeur2) {
+            return valeur1 - valeur2;
+        }
 
-function power() {
-	
-}
+        function power(valeur, puissance) {
 
-function factorial() {
-	
-}
+            if (valeur == 0) return 0;
+            else {
+                if (puissance == 0) return 1;
+                else {
+                    let resultat = 1;
+                    for (let i = 1; i <= Math.abs(puissance); i++) {
+                        resultat *= valeur
+                    }
+                    if (puissance > 0) return resultat;
+                    else return (1 / resultat);
+                }
+            }
+        }
 
-module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+        function factorial(valeur) {
+            if (valeur == 0 || valeur == 1) return 1;
+            else if (valeur < 0) return 0;
+            else {
+                let resultat = 1;
+                for (let i = 1; i <= valeur; i++) {
+                    resultat *= i;
+                }
+                return resultat;
+            }
+        }
